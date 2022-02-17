cda05e46a17a7fd037ac591ec5661b37136e2d5d
caysonjh <hamilton.cayson@gmail.com>
2022-02-17 10:13:06 -0700

    Updating to the newer version of our code

--- RandomWalks.jl
+++ RandomWalks.jl
@@ -1,15 +1,19 @@
 using Plots
-global n = 0 #num steps
-global t = 0 #num walkers
-global d = 0 #num dimensions
-global anim = Animation()
 
-function one()
+"""
+Function to run random walk in 1 dimension
+"""
+function one(n, t, d)
+    anim = Animation()
     x = rand(-1:1, t)
     counter = 1
 
+    """
+    Based on random integer, move the walker one of 2 directions. 
+    Repeat for t amount of items in the array and for n number of steps. 
+    """
     for j in 1:n
-        display(histogram(x, 
+        display(histogram(vec(x), 
         label = "Number of steps: $counter", 
         color = :green, 
         legend_background_color = :gold,
@@ -30,14 +34,23 @@ function one()
         end
         counter += 1
     end
+    
     gif(anim)
 end
 
-function two()
+"""
+Function to run the walk in 2 dimensions
+"""
+function two(n, t, d)
+    anim = Animation()
     x = rand((-1,1), t)
     y = rand((-1,1), t)
     counter = 1
 
+    """
+    Based on random integer, move the walker one of 4 directions by incrementing either the x or y array. 
+    Repeat for t amount of items in the arrays and for n number of steps. 
+    """
     for j in 1:n 
         for i in 1:t 
             test = rand((1:4))
@@ -67,12 +80,20 @@ function two()
     gif(anim)
 end
 
-function three()
+"""
+Function to run a random walk in 3 dimensions
+"""
+function three(n, t, d)
+    anim = Animation()
     x = rand((-1:1), t)
     y = rand((-1:1), t)
     z = rand((-1:1), t)
     counter = 1
 
+    """
+    Based on random integer, move the walker one of 6 directions by incrementing either the x, y, or z array. 
+    Repeat for t amount of items in the arrays and for n number of steps. 
+    """
     for j in 1:n 
         for i in 1:t 
             test = rand((1:6))
@@ -107,19 +128,22 @@ function three()
     gif(anim)
 end
 
+"""
+Function called to run the program. Call RandomWalk(num steps, num walkers, num dimensions)
+"""
 function RandomWalk(arg1::Integer, arg2::Integer, arg3::Integer) 
-    global n = arg1
-    global t = arg2
-    global d = arg3
+    n = arg1
+    t = arg2
+    d = arg3
 
     if n == 0 || t == 0 || d == 0
         println("failed")
     elseif d == 1
-        one()
+        one(n, t, d)
     elseif d == 2
-        two()
+        two(n, t, d)
     else
-        three()
+        three(n, t, d)
     end
     println("Have a nice day!")
 end
